387b7997719f08e3e629b989ee98a62d6816dee1
skybluenecktie <77959300+skybluenecktie@users.noreply.github.com>
2026-03-24 18:44:17 +0800

    Revert "Update App.jsx"
    This reverts commit 4987ef275423bc27d04b132c7df0fdca7a2e66e4.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
 import SKChairmanDashboard from "./pages/SKChairmanDashboard";
@@ -17,83 +16,15 @@ function App() {
     <BrowserRouter>
       <Routes>
          <Route path="/" element={<Login />} />
-        {/* Public Routes */}
-        <Route path="/" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/chairman-dashboard" element={<SKChairmanDashboard />} />
         <Route path="/kagawad-dashboard" element={<SKKagawadDashboard />} />
-        <Route path="/register-youth" element={<RegisterYouth />} />
-        <Route path="/generate-reports" element={<GenerationQR />} />
+        <Route path="/register" element={<RegisterYouth />} />
+        <Route path="/generate" element={<GenerationQR />} />
         <Route path="/scan" element={<ScanQr />} />
-        <Route path="/create-program" element={<CreatePrograms />} />
-        <Route path="/manage-youth" element={<ManageYouth />} />
+        <Route path="/programs" element={<CreatePrograms />} />
+        <Route path="/manageYouth" element={<ManageYouth />} />
         <Route path="/settings" element={<Settings />} />
-
-        {/* Protected Routes */}
-        <Route
-          path="/chairman-dashboard"
-          element={
-            <ProtectedRoute allowedRoles={["chairman"]}>
-              <SKChairmanDashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/kagawad-dashboard"
-          element={
-            <ProtectedRoute allowedRoles={["kagawad"]}>
-              <SKKagawadDashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/register-youth"
-          element={
-            <ProtectedRoute allowedRoles={["chairman", "kagawad"]}>
-              <RegisterYouth />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/generate-reports"
-          element={
-            <ProtectedRoute allowedRoles={["chairman", "kagawad"]}>
-              <GenerationQR />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/scan"
-          element={
-            <ProtectedRoute allowedRoles={["chairman", "kagawad"]}>
-              <ScanQr />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/create-program"
-          element={
-            <ProtectedRoute allowedRoles={["chairman", "kagawad"]}>
-              <CreatePrograms />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/manage-youth"
-          element={
-            <ProtectedRoute allowedRoles={["chairman", "kagawad"]}>
-              <ManageYouth />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/settings"
-          element={
-            <ProtectedRoute allowedRoles={["chairman", "kagawad"]}>
-              <Settings />
-            </ProtectedRoute>
-          }
-        />
       </Routes>
     </BrowserRouter>
   );
